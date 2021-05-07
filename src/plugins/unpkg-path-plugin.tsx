47aa5ee60148c424b0dbc09edbc76ec7c202a271
lukedxvxes <lukedaviesweb+1@gmail.com>
2021-05-07 21:38:41 +0800

    refactores onload to its own fetch-plugin

--- src/plugins/unpkg-path-plugin.tsx
+++ src/plugins/unpkg-path-plugin.tsx
@@ -1,10 +1,4 @@
 import * as esbuild from 'esbuild-wasm'
-import axios from 'axios'
-import localForage from 'localforage'
-
-const fileCache = localForage.createInstance({
-	name: 'filecache',
-})
 
 export const unpkgPathPlugin = (inputCode: string) => {
 	return {
@@ -29,36 +23,6 @@ export const unpkgPathPlugin = (inputCode: string) => {
 					namespace: 'a',
 					path: `https://unpkg.com/${args.path}`,
 				}
-			})
-
-			build.onLoad({ filter: /.*/ }, async (args: any) => {
-				console.log('onLoad', args)
-				if (args.path === 'index.js') {
-					return {
-						loader: 'jsx',
-						contents: inputCode,
-					}
-				}
-
-				const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
-					args.path,
-				)
-
-				if (cachedResult) {
-					return cachedResult
-				}
-
-				const { data, request } = await axios.get(args.path)
-
-				const result: esbuild.OnLoadResult = {
-					loader: 'jsx',
-					contents: data,
-					resolveDir: new URL('./', request.responseURL).pathname,
-				}
-
-				await fileCache.setItem(args.path, result)
-
-				return result
 			})
 		},
 	}
