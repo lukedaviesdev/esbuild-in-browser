49c20321bb8eaf73a2935348610acc93f09d56d2
lukedxvxes <lukedaviesweb+1@gmail.com>
2021-05-07 22:36:58 +0800

    adds css imports

--- src/plugins/fetch-plugin.tsx
+++ src/plugins/fetch-plugin.tsx
@@ -9,14 +9,14 @@ export const fetchPlugin = (inputCode: string) => {
 	return {
 		name: 'fetch-plugin',
 		setup(build: esbuild.PluginBuild) {
-			build.onLoad({ filter: /.*/ }, async (args: any) => {
-				if (args.path === 'index.js') {
-					return {
-						loader: 'jsx',
-						contents: inputCode,
-					}
+			build.onLoad({ filter: /(^index\.js$)/ }, async (args: any) => {
+				return {
+					loader: 'jsx',
+					contents: inputCode,
 				}
+			})
 
+			build.onLoad({ filter: /.*/ }, async (args: any) => {
 				const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
 					args.path,
 				)
@@ -24,7 +24,32 @@ export const fetchPlugin = (inputCode: string) => {
 				if (cachedResult) {
 					return cachedResult
 				}
+			})
 
+			build.onLoad({ filter: /.css$/ }, async (args: any) => {
+				const { data, request } = await axios.get(args.path)
+				const escaped = data
+					.replace(/\n/g, '')
+					.replace(/"/g, '\\"')
+					.replace(/'/g, "\\'")
+
+				const contents = `
+                    const style = document.createElement('style');
+                    style.innerText = '${escaped}';
+                    document.head.appendChild(style);
+                `
+				const result: esbuild.OnLoadResult = {
+					loader: 'jsx',
+					contents,
+					resolveDir: new URL('./', request.responseURL).pathname,
+				}
+
+				await fileCache.setItem(args.path, result)
+
+				return result
+			})
+
+			build.onLoad({ filter: /.*/ }, async (args: any) => {
 				const { data, request } = await axios.get(args.path)
 
 				const result: esbuild.OnLoadResult = {
