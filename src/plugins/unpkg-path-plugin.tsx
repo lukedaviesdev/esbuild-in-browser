0408721cc3cf70d3af42584763ad789daee8b2b2
lukedxvxes <lukedaviesweb+1@gmail.com>
2021-05-07 21:31:06 +0800

    resolve refactor

--- src/plugins/unpkg-path-plugin.tsx
+++ src/plugins/unpkg-path-plugin.tsx
@@ -10,22 +10,21 @@ export const unpkgPathPlugin = (inputCode: string) => {
 	return {
 		name: 'unpkg-path-plugin',
 		setup(build: esbuild.PluginBuild) {
+			//handle root entry file
+			build.onResolve({ filter: /(^index\.js$)/ }, (args: any) => {
+				return { path: 'index.js', namespace: 'a' }
+			})
+			// handle rel paths in module :  includes ./ || ../
+			build.onResolve({ filter: /^\.+\// }, (args: any) => {
+				const url = new URL(args.path, `https://unpkg.com${args.resolveDir}/`)
+
+				return {
+					path: url.href,
+					namespace: 'a',
+				}
+			})
+			// handle main file
 			build.onResolve({ filter: /.*/ }, async (args: any) => {
-				console.log('onResolve', args)
-
-				if (args.path === 'index.js') {
-					return { path: args.path, namespace: 'a' }
-				}
-
-				if (args.path.includes('./') || args.path.includes('../')) {
-					const url = new URL(args.path, `https://unpkg.com${args.resolveDir}/`)
-
-					return {
-						path: url.href,
-						namespace: 'a',
-					}
-				}
-
 				return {
 					namespace: 'a',
 					path: `https://unpkg.com/${args.path}`,
