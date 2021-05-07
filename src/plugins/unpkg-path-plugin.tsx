3edfec5db4b8a56892aa2ff0ee96a9b8245e0528
lukedxvxes <lukedaviesweb+1@gmail.com>
2021-05-07 21:09:19 +0800

    adds caching layer using localForage

--- src/plugins/unpkg-path-plugin.tsx
+++ src/plugins/unpkg-path-plugin.tsx
@@ -1,16 +1,18 @@
 import * as esbuild from 'esbuild-wasm'
 import axios from 'axios'
+import localForage from 'localforage'
+
+const fileCache = localForage.createInstance({
+	name: 'filecache',
+})
 
 export const unpkgPathPlugin = () => {
 	return {
 		name: 'unpkg-path-plugin',
 		setup(build: esbuild.PluginBuild) {
-			// finding the file
 			build.onResolve({ filter: /.*/ }, async (args: any) => {
 				console.log('onResolve', args)
 
-				// esbuild epects you to return an object,
-				// then uses this object in build.onLoad
 				if (args.path === 'index.js') {
 					return { path: args.path, namespace: 'a' }
 				}
@@ -30,28 +32,35 @@ export const unpkgPathPlugin = () => {
 				}
 			})
 
-			// loading the file, using the filepath
-			// returned by build.onResolve()
 			build.onLoad({ filter: /.*/ }, async (args: any) => {
 				console.log('onLoad', args)
-				// hyjacks the loading of the file and returns an
-				// object, in the shape esbuild is expecting, with
-				// the custom contents
 				if (args.path === 'index.js') {
 					return {
 						loader: 'jsx',
-						contents: `import react from 'react@16.0.0';
-							console.log(react);`,
+						contents: `import axios from 'axios';
+							console.log(axios);`,
 					}
+				}
+
+				const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+					args.path,
+				)
+
+				if (cachedResult) {
+					return cachedResult
 				}
 
 				const { data, request } = await axios.get(args.path)
 
-				return {
+				const result: esbuild.OnLoadResult = {
 					loader: 'jsx',
 					contents: data,
 					resolveDir: new URL('./', request.responseURL).pathname,
 				}
+
+				await fileCache.setItem(args.path, result)
+
+				return result
 			})
 		},
 	}
